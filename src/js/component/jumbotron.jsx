51a8e939fb8728657a7e3f2eae76346904bfa375
Ender Calderón <177250779+Yohebyth@users.noreply.github.com>
2024-11-08 18:50:54 +0000

    first commit

---
+++ src/js/component/jumbotron.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Jumbotron = () => {
+    return (
+        <div  className="jumbotron">
+            <h1>A Warm Welcome!</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi est doloremque, aperiam odit animi illo nobis necessitatibus dolor, sunt maxime reprehenderit natus totam quae quos, consequuntur ad dicta deleniti quam.</p>
+            <p><a href="#" className="btn btn-primary">Call to acction!</a></p>
+        </div>
+    )
+}
+
+export default Jumbotron;
